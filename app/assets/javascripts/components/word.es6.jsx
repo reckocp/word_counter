a8c9ffafec55254bc7a19c47c1fcfb43fa3f742d
COLIN <reckocp@gmail.com>
2016-09-28 17:35:42 -0500

    React WUT

--- app/assets/javascripts/components/word.es6.jsx
+++ app/assets/javascripts/components/word.es6.jsx
@@ -12,21 +12,26 @@ class Word extends React.Component {
     newState["clicked"] = true;
     this.setState(newState);
   }
+  _untouchedMe (){
+    var oldState = {};
+    oldState["clicked"] = true;
+    this.setState(oldState);
+  }
   render () {
     let open = <a onClick={this._touchedMe.bind(this)}>Show</a>;
 
     if(this.state.clicked){
       open =       <div>
               <h2> {this.props.word.sentence} </h2>
-              <p> {this.props.sentenceCount} </p>
-              <p> {this.props.wordCount} </p>
-              <p> {this.props.charCount} </p>
-              <p> {this.props.nounCount} </p>
-              <p> {this.props.verbCount} </p>
-              <p> {this.props.adjCount} </p>
-              <p> {this.props.detCount} </p>
-              <p> {this.props.punctCount} </p>
-              <p> {this.props.mostCommonCharCount} </p>
+              <p> Number of sentences: {this.props.sentenceCount} </p>
+              <p> Number of words: {this.props.wordCount} </p>
+              <p> Number of characters: {this.props.charCount} </p>
+              <p> Number of nouns: {this.props.nounCount} </p>
+              <p> Number of verbs: {this.props.verbCount} </p>
+              <p> Number of adjectives: {this.props.adjCount} </p>
+              <p> Number of determiners: {this.props.detCount} </p>
+              <p> Number of punctuation marks: {this.props.punctCount} </p>
+              <p> Most common character used: {this.props.mostCommonCharCount} </p>
             </div>
     }
     return (
